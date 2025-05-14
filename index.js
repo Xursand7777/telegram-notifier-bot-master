1deb08fed44fde3c2d4ab855936eba7548595a4f
Khursandbek <khursand.khaitbaev@mail.ru>
2025-05-15 00:37:36 +0500

    change message

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: false });
 
 ;(async () => {
   const groupIds = await getGroupIds();
-  const message = `🚀 Hello from GitHub Actions! (${new Date().toLocaleString()})`;
+  const message = `
+  SADA ELD (Logbook) SERVICE
+
+✅ As a client of ours, you will have access to the following services:
+
+🟢 Audit
+
+🟢 Monitoring
+
+🟢 24/7 ELD support
+
+🟢 Consulting service
+
+🟢 Fixing Profile forms and BOLs
+
+‼️ Currently, We have 2 weeks of FREE- trial, Contact us to try our high quality service for totally free! ‼️
+
+For more info: @sada_manager
+📞 (234) 413-1826 🇺🇸
+📩 sadaeldservice@gmail.com
+  `;
 
   if (!groupIds.length) {
     console.log("⚠️ No group IDs found, nothing to send.");
